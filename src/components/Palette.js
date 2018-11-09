4021b8221ee02cbf638cfb37475f04b6abab0432
ewgalassi <danza_seppia@yahoo.com>
2018-11-09 13:09:10 -0600

    added color gradients

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -3,13 +3,18 @@ import "./Palette.css";
 
 class Palette extends Component {
 
-
-    render() {
-        return (
-          <div className="palette content">
-          </div>
-        );
-      }
+  render() {
+    return (
+      <div className="palette content">
+        <div className="shades"></div>
+        <div className="colors1"></div>
+        <div className="colors2"></div>
+        <div className="colors3"></div>
+        <div className="colors4"></div>
+        <div className="selected"></div>
+      </div>
+    );
+  }
 }
 
 export default Palette;
